785c96fa8bd779a621ef290ce4fe2a6969fe088f
ejdam87 <adam.dzadon@gmail.com>
2024-09-27 09:53:15 +0300

    feature: cv front-end add

---
+++ static/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.sidenav');
+    let instances = M.Sidenav.init(elems);
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems);
+});
